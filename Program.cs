2492602502c9bd2619c5121b5f78b64f9d021484
samira hamada <samirahamadaabdelaleem123@gmail.com>
2026-09-06 15:31:55 +0300

    #region 8th Question   //Given string yearText = "2023";, convert it using int.Parse(). Then given string badText = "abc";, use int.TryParse() to safely try converting it, and print "Invalid number" if it fails.
      string yearText = "2023";
    
      int year = int.Parse(yearText);
    
      Console.WriteLine(year);
    
      string badText = "abc";
    
      if (int.TryParse(badText, out int number))
      {
          Console.WriteLine(number);
      }
      else
      {
          Console.WriteLine("Invalid number");
      }
      #endregion

--- Program.cs
+++ Program.cs
@@ -75,6 +75,26 @@
                 Console.WriteLine(pagesInt);
 
                 #endregion
+                #region 8th Question
+                //Given string yearText = "2023";, convert it using int.Parse(). Then given string badText = "abc";, use int.TryParse() to safely try converting it, and print "Invalid number" if it fails.
+
+                string yearText = "2023";
+
+                int year = int.Parse(yearText);
+
+                Console.WriteLine(year);
+
+                string badText = "abc";
+
+                if (int.TryParse(badText, out int number))
+                {
+                    Console.WriteLine(number);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid number");
+                }
+                #endregion
 
 
 
