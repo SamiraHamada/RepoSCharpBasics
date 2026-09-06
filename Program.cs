9ac7c5d72421c22ba30de858bba1fc65fc3decb6
samira hamada <samirahamadaabdelaleem123@gmail.com>
2026-09-06 15:07:52 +0300

    #region 1st Question   //Create a Book class with a Title (string) and Pages (int). Create a Book object and store it in a variable of type object. Print it.
      Book book = new Book();
      book.Title = "The Road To Success";
      book.Pages = 200;
      Object obj = book;
      Console.WriteLine(obj);
    
      #endregion

--- Program.cs
+++ Program.cs
@@ -1,10 +1,30 @@
 ﻿namespace Assignment01_C_Basics
 {
-    internal class Program
+    class Book
     {
-        static void Main(string[] args)
+        public string Title;
+        public int Pages;
+
+        public override string ToString()
         {
-           
+            return $"{Title} ,{Pages}";
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                #region 1st Question
+                //Create a Book class with a Title (string) and Pages (int). Create a Book object and store it in a variable of type object. Print it.
+
+                Book book = new Book();
+                book.Title = "The Road To Success";
+                book.Pages = 200;
+                Object obj = book;
+                Console.WriteLine(obj);
+
+                #endregion
+
+            }
         }
     }
 }
